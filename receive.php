ada6a2a29869d38cf32c45d3a9ec8df4319e800d
krishandarji <kd283@njit.edu>
2017-03-29 17:46:16 -0400

    RabbitMQ send + receive

---
+++ receive.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$ip_addr = '192.168.0.110';
+
+$connection = new AMQPStreamConnection($ip_addr, 5672, 'admin', 'password');
+$channel = $connection->channel();
+
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function($msg) {
+  echo " [x] Received ", $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
+
+?>
---
+++ send.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$rec_addr = '192.168.0.110';
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
+
+?>
